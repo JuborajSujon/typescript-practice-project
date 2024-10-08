390e9b0fc5b42c555e4c6989373a567a759dc9e9
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-08 10:40:17 +0600

    Added object in ts

--- dave-gray/build/js/non-primitive.js
+++ dave-gray/build/js/non-primitive.js
@@ -12,3 +12,32 @@ let bands = [];
 bands.push("Van Halen");
 // Tuple
 let myTuple = ["Dave", 32, true];
+myTuple[1] = 42;
+// Object
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Dave",
+    prop2: 32,
+    prop3: true,
+    prop4: ["John", "Jane"],
+};
+let evh = {
+    name: "Eddie",
+    active: false,
+    albums: [1984, 5150, "OU812"],
+};
+let jp = {
+    name: "John",
+    active: true,
+    albums: [1984, 5150, "OU812"],
+};
+// evh = jp;
+console.log(evh);
+const greetGuitarist = (guitarist) => {
+    return `Hello ${guitarist.name}`;
+};
+console.log(greetGuitarist(jp));
--- dave-gray/src/non-primitive.ts
+++ dave-gray/src/non-primitive.ts
@@ -17,3 +17,48 @@ bands.push("Van Halen");
 
 // Tuple
 let myTuple: [string, number, boolean] = ["Dave", 32, true];
+
+myTuple[1] = 42;
+
+// Object
+let myObj: object;
+myObj = [];
+console.log(typeof myObj);
+
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Dave",
+  prop2: 32,
+  prop3: true,
+  prop4: ["John", "Jane"],
+};
+
+interface Guitarist {
+  name: string;
+  active: boolean;
+  albums: (string | number)[];
+}
+
+let evh: Guitarist = {
+  name: "Eddie",
+  active: false,
+  albums: [1984, 5150, "OU812"],
+};
+
+let jp: Guitarist = {
+  name: "John",
+  active: true,
+  albums: [1984, 5150, "OU812"],
+};
+
+// evh = jp;
+
+console.log(evh);
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.name}`;
+};
+
+console.log(greetGuitarist(jp));
