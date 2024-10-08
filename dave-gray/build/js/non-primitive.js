91b5236d8949a277b80429ea239b6b24942cc5fe
Juboraj Sujon <mdsujon72@gmail.com>
2024-10-08 10:08:27 +0600

    Added array defination

---
+++ dave-gray/build/js/non-primitive.js
@@ -0,0 +1,14 @@
+"use strict";
+let stringArr = ["John", "Jane", "Jack", "Jill"];
+let guitars = ["Strat", "Les Paul", 5150];
+let mixed = ["John", 5, true];
+stringArr[0] = "Tom";
+stringArr.push("Tom");
+guitars[0] = 1984;
+guitars.unshift("Strat");
+let test = [];
+test.push(5);
+let bands = [];
+bands.push("Van Halen");
+// Tuple
+let myTuple = ["Dave", 32, true];
---
+++ dave-gray/src/non-primitive.ts
@@ -0,0 +1,19 @@
+let stringArr = ["John", "Jane", "Jack", "Jill"];
+
+let guitars = ["Strat", "Les Paul", 5150];
+
+let mixed = ["John", 5, true];
+
+stringArr[0] = "Tom";
+stringArr.push("Tom");
+guitars[0] = 1984;
+guitars.unshift("Strat");
+
+let test = [];
+test.push(5);
+
+let bands: string[] = [];
+bands.push("Van Halen");
+
+// Tuple
+let myTuple: [string, number, boolean] = ["Dave", 32, true];
