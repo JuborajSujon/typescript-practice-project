85947227571fa85857b396ba98c8a72e0a30d85e
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-05 21:53:21 +0600

    typeof

--- PH/ms1/conceptualSecond/src/typeof.ts
+++ PH/ms1/conceptualSecond/src/typeof.ts
@@ -19,11 +19,33 @@
     director: "Mir vai",
   }
 
-
+// in guard check 
   type DetailsLog = {
     message: string;
     level: "info" | "warn" | "error";
   }
+
+  function logMessage(input: string | number | DetailsLog) {
+    if (typeof input === 'string') {
+      console.log("simple message info", input);
+    }
+
+    else if (typeof input === 'number') {
+      console.log("Error code:", input);
+    }
+    else if ("message" in input && "level" in input) {
+      console.log(`danger level ${input.level} instructions for next level dev ${input.message}`)
+    }
+  }
+
+  // const test = logMessage("string type er input");
+  // const test = logMessage(23);
+  const test = logMessage({
+    message: "Vai ready to",
+    level: "error"
+  });
+
+  console.log(test)
 
 
   // 
