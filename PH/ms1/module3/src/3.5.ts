b5653a0050ad977d6b932283a27c72347351b8ca
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-08 22:57:32 +0600

    access modifier

---
+++ PH/ms1/module3/src/3.5.ts
@@ -0,0 +1,43 @@
+{
+  // Access Modifiers
+
+  class BankAccount{
+   public readonly id: number;
+   protected name: string;
+   private _balance: number;
+
+
+    constructor(id: number, name: string, balance: number) {
+      this.id = id;
+      this.name = name;
+      this._balance = balance;
+    }
+
+    addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+    getBalance() {
+      return this._balance;
+    }
+  }
+
+  class StudentAccount extends BankAccount{
+    test() {
+      this.name;
+    }
+  }
+
+  const poorAccout = new BankAccount(111, "Mr. Poor", 20);
+
+  poorAccout.addDeposit(300);
+  const myBalance = poorAccout.getBalance();
+  console.log(myBalance);
+
+
+
+  
+
+
+  // 
+}
