bccdfb596c0d1d95292811e4611f6758ddf8e514
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-09 12:50:27 +0600

    basic types

---
+++ PH/ms1/module1/pone/1.4.ts
@@ -0,0 +1,34 @@
+{
+  //Basic data type
+
+  // string 
+  let firstName: string = "mezba";
+
+  // number
+  let roll: number = 123;
+
+  // boolean 
+  let isAdmin: boolean = true;
+
+  // undefined 
+  let x: undefined = undefined;
+
+  // null
+  let y: null = null;
+
+  let d: number;
+  d = 123;
+
+  // Array 
+  let friends: string[] = ["sujon", "shohag"];
+  let eligibleRollList: number[] = [1, 2, 3];
+
+  // Tuple --> special array --> order --> type of value
+
+  let coordinates: [number, number] = [1, 5];
+
+
+
+  
+  // 
+}
