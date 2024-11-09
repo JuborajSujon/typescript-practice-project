f5dfc74b8475fef9386fcfc3224bf43d7f661e77
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-09 14:12:19 +0600

    destructuring

---
+++ PH/ms1/module1/pone/1.8.ts
@@ -0,0 +1,24 @@
+{
+  // destructuring
+
+  // object destructuring 
+  const user = {
+    id: 345,
+    name: {
+      firstName: "Sujon",
+      middleName: "Abedin",
+      lastName: "Persian"
+    },
+    contactNo: "0170000000",
+    address: "Uganda"
+  }
+
+
+  const { contactNo, name: { middleName: midName } } = user;
+  
+
+  // array destructuring
+  const myFriends = ["chandler", 'joey', "ross", "rachel"];
+
+  const [,, bestFriend, ...rest] = myFriends;
+}
