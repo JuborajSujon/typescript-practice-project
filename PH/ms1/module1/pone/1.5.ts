174fb87e460333ef68dda1cabaa83c82f808db06
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-09 13:05:52 +0600

    object

--- PH/ms1/module1/pone/1.5.ts
+++ PH/ms1/module1/pone/1.5.ts
@@ -1,5 +1,17 @@
 {
-  //
+  //Reference type --> object
+
+  const user: {
+    firstName: string;
+    middleName?: string;
+    lastName: string;
+    isMarried: boolean;
+  } = {
+    firstName: "Juboraj",
+    // middleName: "hossen",
+    lastName: "sujon",
+    isMarried: true,
+  }
 
   
   // 
